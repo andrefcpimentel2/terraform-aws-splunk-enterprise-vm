f595e334217f3b3043c27fbbd62721011712e200
andrefcpimentel2 <andre@hashicorp.comp>
2024-12-11 10:07:36 +0100

    using socker in ubuntu image

--- server.tf
+++ server.tf
@@ -1,25 +1,25 @@
-data "aws_ami" "splunk" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["679593333241"] ## Splunk Account 
 
+# ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*
+#ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*
   filter {
     name   = "name"
-    values = ["splunk_AMI*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+    # values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    #  values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+      values = ["ubuntu/images/*ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
+  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "splunk_ent" {
-  ami           = data.aws_ami.splunk.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type_server
   key_name      = aws_key_pair.demostack.id
 
@@ -30,4 +30,27 @@ resource "aws_instance" "splunk_ent" {
     device_index         = 0
   }
   availability_zone = data.aws_availability_zones.AZ.names[0]
+
+  user_data = data.cloudinit_config.servers.rendered
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+# Gzip cloud-init config
+data "cloudinit_config" "servers" {
+
+  gzip          = true
+  base64_encode = true
+
+  #base
+  part {
+    content_type = "text/x-shellscript"
+    content      = templatefile("splunk.sh",{
+        password = random_password.password.result
+    })
+   }
 }
---
+++ splunk.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -x
+
+echo "==> Base"
+
+
+echo "--> Installing common dependencies"
+sudo apt-get update
+apt-get install -y \
+  build-essential \
+  cpu-checker \
+  nodejs \
+  curl \
+  emacs \
+  git \
+  jq \
+  tmux \
+  unzip \
+  vim \
+  wget \
+  tree \
+  qemu-kvm \
+  virt-manager \
+  virtinst \
+  libvirt-clients \
+  bridge-utils \
+  libvirt-daemon-system \
+  apt-transport-https \
+  curl \
+  &>/dev/null
+
+
+echo "==> Install Docker"
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "==> Run Splunk Container"
+
+docker pull splunk/splunk:latest
+
+docker run -d -p 8000:8000 -e SPLUNK_START_ARGS='--accept-license' -e SPLUNK_PASSWORD='${password}' splunk/splunk:latest
+
